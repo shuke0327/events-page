2f65f3ed23cdff1b4199c58cd35045fc5beb7704
shuke0327 <jingkai64@gmail.com>
2016-12-27 22:56:21 +0800

    add todo controller and model

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,3 +1,34 @@
 class TodosController < ApplicationController
-  
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo.update_attributes(:todo_params)
+  end
+
+  def change_assign_state
+    @todo.change_assign
+  end
+
+  def change_due_at
+    @todo.change_due_at
+  end
+
+  def drop
+    #softdestroy
+  end
+
+  def recover
+  end
+
+  def finished
+    @todo.finished
+  end
+
+  def reopen
+    @todo.reopen
+  end
+
+
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,28 @@
 class Todo < ApplicationRecord
+   include Wisper.model
+
+  def create
+  end
+
+  def soft_destroy
+  end
+
+  def redo
+  end
+
+  def complete
+  end
+
+  def reopen
+  end
+
+  def assigned_to(user)
+  end
+
+  def set_due_at(time)
+  end
+
+  def comment
+  end
+
 end
